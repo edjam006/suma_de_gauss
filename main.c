702a714381adea4e9f7f2bb63e474db344b85a44
Eduardo Andrade <ejac09102006@gmail.com>
2023-10-16 16:07:17 -0500

    Operacion del metodo de Gauss agregada

--- main.c
+++ main.c
@@ -2,5 +2,12 @@
 
 int main(void){
     int sum, i, num;
-    
+    printf ("Ingrese un numero entero\n");
+    scanf("%d", &num);
+    if (num < 0){
+        printf("Ingrese un numero entero positivo\n");
+    } else {
+        sum= (num * (num + 1))/2;
+    }
+
 }
