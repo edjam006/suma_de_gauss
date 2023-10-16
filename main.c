d6d77544da97e1116523af371506c33434b835f5
Eduardo Andrade <ejac09102006@gmail.com>
2023-10-16 16:16:58 -0500

    Operacion por medio de un for

--- main.c
+++ main.c
@@ -4,11 +4,19 @@ int main(void){
     int sum, i, num;
     printf ("Ingrese un numero entero\n");
     scanf("%d", &num);
-    if (num < 0){
+    /*if (num < 0){
         printf("Ingrese un numero entero positivo\n");
     } else {
         sum= (num * (num + 1))/2;
         printf("La suma de Gauss del numero %d es: %d", num, sum);
     }
-
+    */
+   if (num < 0){
+    printf("Ingrese un numero entero positivo\n");
+   } else {
+    for (i= 1; i <= num; i++){
+        sum= sum + i;
+    }
+    printf("La suma de Gauss del numero %d es: %d\n", num, sum);
+   }
 }
